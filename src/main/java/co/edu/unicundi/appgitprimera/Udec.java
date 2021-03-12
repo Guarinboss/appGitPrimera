5dbc4148987477579221d5ec4eca9fb8ac7238da
Guarinboss <cenefo2706@gmail.com>
2021-03-12 07:44:51 -0500

    Se agrego variables y constructor vacio

--- src/main/java/co/edu/unicundi/appgitprimera/Udec.java
+++ src/main/java/co/edu/unicundi/appgitprimera/Udec.java
@@ -11,4 +11,29 @@ package co.edu.unicundi.appgitprimera;
  */
 public class Udec {
     
+    private String nombre; 
+    
+    private String ubicacion; 
+
+    public Udec() {
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getUbicacion() {
+        return ubicacion;
+    }
+
+    public void setUbicacion(String ubicacion) {
+        this.ubicacion = ubicacion;
+    }
+    
+    
+    
 }
